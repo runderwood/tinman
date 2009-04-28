664c84299c4b10d94927180207fd4d12c14cbb9f
Reed Underwood <reed@jellybones.(none)>
2009-04-28 14:36:06 -0500

    Incrementals...hw.

--- tm.php
+++ tm.php
@@ -8,12 +8,19 @@ define('UFILE', '.u.json');
 define('STORE', DATADIR.'store/');
 define('HPRE', 'h_');
 define('AUTHREALM', 'tinman');
+
+//app-specific constants
+define('HOMEWORK_EXT', 'hw.json');
+define('HOMEWORK_DIR', STORE.'homework/');
+define('HOMEWORK_URI', 'homework/');
+
 $global_file_cache = array(); // per-request file contents cache.
 $routes = array(
-    'about' => 'about',
-    'store\/.*' => 'store'
+    'current-user' => 'current_user',
+    'homework(\/.*)?' => 'homework'
 );
-//TODO: dir manifests w/ perms.
+//TODO: dir manifests w/ perms.?
+//TODO: content negotiation?
 $users = array(
     'admin' => array(
         'salt' => 'saysthetinman',
@@ -42,6 +49,12 @@ function auth_valid_username($un) {
     return preg_match('/^\w{4,24}$/', $un);
 }
 
+function auth_user_dir($un) {
+    $subdir = substr($un, 0, 2).'/';
+    $subdir .= $un.'/';
+    return file_exists(USERDIR.$subdir) ? USERDIR.$subdir : false;
+}
+
 function auth_ufile_load($un) {
     $subdir = substr($un, 0, 2).'/';
     $subdir .= $un.'/';
@@ -68,6 +81,7 @@ function auth_user() {
         $pw = $_SERVER['PHP_AUTH_PW'];
         if(auth_user_exists($un) && auth_test_pw($un, $pw)){
             $user = auth_user_exists($un);
+            if(!isset($user->un)) $user->un = $un;
         } else {
             $user = false;
         }
@@ -159,101 +173,38 @@ function file_load($p, $c=true) {
     return $f;
 }
 
-function store_get_path($uri) {
-    $tok = explode('/', rtrim($uri, '/'));
-    array_shift($tok);
-    $p = STORE.join('/', $tok);
+function homework_get_path() {
+    $p = HOMEWORK_DIR;
     return $p;
 }
 
-function store_get_type($p) {
-    $tok = explode('/', $p);
-    $fn = array_pop($tok);
-    $tok = explode('.', $fn);
-    $ext = strtolower(array_pop($tok));
-    $ct = 'text/plain';
-    $cts = array(
-        'json' => 'application/json',
-        'txt' => 'text/plain',
-        'html' => 'text/html',
-        'xml' => 'application/xml',
-        'css' => 'text/css',
-        'yaml' => 'text/yaml'
-    );
-    if(array_key_exists($ext, $cts)) {
-        $ct = $cts[$ext];
+function homework_gen_fn($un, $ts=null) {
+    if($un) {
+        global $user;
+        $un = $user->un;
     }
-    return $ct;
+    $ts = $ts ? $ts : time();
+    $fn = substr(md5($un.$ts.'THEDOGATEIT'), 0, 8);
+    return $fn.'.'.HOMEWORK_EXT;
+}
+
+function homework_get_uri_fn($uri) {
+    $uri = rtrim($uri, '/');
+    $fn = false;
+    if(preg_match('/'.str_replace('/', '\/', HOMEWORK_URI).'[a-f0-9]{8}\.'.str_replace('.', '\.', HOMEWORK_EXT).'$/', $uri)) {
+        $fn = preg_replace('/.*\/([a-f0-9]{8}\.'.str_replace('.', '\.', HOMEWORK_EXT).')$/', '$1', $uri);
+    }
+    return $fn;
+}
+
+function homework_load($fn) {
+    $p = HOMEWORK_DIR;
+    $hw = file_load($p.$fn);
+    if($hw) $hw = json_decode($hw);
+    return $hw;
 }
 
 // handlers
-function h_get_about() {
-    print "This is the Tinman.\n";
-    return true;
-}
-
-function h_post_about() {
-    print file_get_contents("php://input");
-    print "\n\n";
-    return true;
-}
-
-function h_get_store() {
-    global $uri;
-    $p = store_get_path($uri);
-    if(file_load($p)) {
-        if(req_header('if-none-match') === etag_file($p)) {
-            header('HTTP/1.1 304 Not Modified');
-        } else {
-            header('Content-Type: '.store_get_type($p));
-            $c = file_load($p);
-            header('Etag: '.etag($c));
-            print $c;
-        }
-    } else {
-        header('HTTP/1.1 404 Not Found');
-        header('Content-Type: text/plain');
-        print "That resource does not exist or is unavailable.\n";
-    }
-}
-
-function h_put_store() {
-    if(!auth_is('admin')) auth_challenge();
-    global $uri;
-    $p = store_get_path($uri);
-    $ptok = explode('/', $p);
-    $fn = array_pop($ptok);
-    $p = join('/', $ptok).'/';
-    $b = file_get_contents("php://input");
-    if(is_writable($p)) {
-        if(req_header('if-match') && is_readable($p.$fn)) {
-            if(req_header('if-match') !== etag_file($p.fn)) {
-                header('HTTP/1.1 412 Precondition Failed');
-                header('Content-Type: text/plain');
-                print "Entity tag mismatch.\n";
-            }
-        } else {
-            if(@file_put_contents($p.$fn, $b, LOCK_EX) !== false) {
-                global $global_file_cache;
-                if(isset($global_file_cache[$p.$fn])) {
-                    unset($global_file_cache[$p.$fn]);
-                }
-                header('HTTP/1.1 201 Created');
-                header('Etag: '.etag_file($p.$fn));
-                header('Content-Type: text/plain');
-                print rtrim($uri, '/')."\n";
-            } else {
-                header('HTTP/1.1 500 Internal Server Error');
-                header('Content-Type: text/plain');
-                print "Could not save.\n";
-            }
-        }
-    } else {
-        header('HTTP/1.1 403 Forbidden');
-        print "Resource is immutable.\n";
-    }
-    return true;
-}
 
 function h_delete_store() {
     if(!auth_is('admin')) auth_challenge();
@@ -279,9 +230,143 @@ function h_delete_store() {
     }
 }
 
+function h_get_current_user() {
+    global $user;
+    $ud = new stdClass();
+    $ud->un = $user->un;
+    $ud->email = $user->email;
+    $ud->roles = $user->roles;
+    header('Content-Type: application/json');
+    print json_encode($ud)."\n";
+}
+
+function h_get_homework() {
+    global $uri;
+    if(!($fn = homework_get_uri_fn($uri))) {
+        header('HTTP/1.1 404 Not Found');
+        header('Content-Type: text/plain');
+        print "Not found.\n";
+        return;
+    }
+    $b = file_load(HOMEWORK_DIR.$fn);
+    if(!$b) {
+        header('HTTP/1.1 500 Internal Server Error');
+        header('Content-Type: text/plain');
+        print "Could not load.\n";
+    } else {
+        header('Content-Type: application/json');
+        print $b."\n";
+    }
+}
+
+function h_post_homework() {
+    global $user, $uri, $input_stream;
+    if(!$user) auth_challenge();
+    $p = HOMEWORK_DIR;
+    $hw = false;
+    if($fn = homework_get_uri_fn($uri)) {
+        if(!($hw = homework_load($fn))) {
+            header('HTTP/1.1 404 Not Found');
+            header('Content-Type: text/plain');
+            print "Invalid assignment.\n";
+            return false;
+        }
+    } else {
+        $fn = homework_gen_fn($user->un);
+    }
+    $b = file_get_contents($input_stream);
+    $bd = @json_decode($b);
+    if(!$bd) {
+        header('HTTP/1.1 400 Bad Request');
+        header('Content-Type: text/plain');
+        print "Invalid assignment data.\n";
+        return false;
+    }
+    if($hw && isset($hw->author) && $user->un === $hw->author) {
+        $hw->modified = time();
+        $hw->assignment = $bd;
+        $b = json_encode($hw);
+    } else {
+        $b = json_encode(array(
+            'author' => $user->un,
+            'created' => time(),
+            'modified' => time(),
+            'assignment' => $bd
+        ));
+    }
+    if(is_writable($p)) {
+        if(req_header('if-match') && is_readable($p.$fn)) {
+            if(req_header('if-match') !== etag_file($p.fn)) {
+                header('HTTP/1.1 412 Precondition Failed');
+                header('Content-Type: text/plain');
+                print "Entity tag mismatch.\n";
+            }
+        } else {
+            if(@file_put_contents($p.$fn, $b, LOCK_EX) !== false) {
+                global $global_file_cache;
+                if(isset($global_file_cache[$p.$fn])) {
+                    unset($global_file_cache[$p.$fn]);
+                }
+                $status = $hw ? '200 Accepted' : '201 Created';
+                header('HTTP/1.1 '.$status);
+                if(!$hw) header('Location: '.HOMEWORK_URI.$fn);
+                header('Etag: '.etag_file($p.$fn));
+                header('Content-Type: text/plain');
+                print HOMEWORK_URI.$fn."\n";
+            } else {
+                header('HTTP/1.1 500 Internal Server Error');
+                header('Content-Type: text/plain');
+                print "Could not save.\n";
+            }
+        }
+    } else {
+        header('HTTP/1.1 403 Forbidden');
+        print "Resource is immutable.($p)\n";
+    }
+    return true;
+}
+
+function h_delete_homework() {
+    global $uri, $user;
+    if(!$user) auth_challenge();
+    $p = HOMEWORK_DIR;
+    $fn = homework_get_uri_fn($uri);
+    if($hw = homework_load($fn)) {
+        if(!(isset($hw->author) && $hw->author == $user->un)) {
+            header('HTTP/1.1 403 Forbidden');
+            header('Content-Type: text/plain');
+            print "You don't have permission to delete that.\n";
+            return;
+        }
+        if(req_header('if-match')) {
+            if(req_header('if-match') !== etag_file($p)) {
+                header('HTTP/1.1 412 Precondition Failed');
+                header('Content-Type: text/plain');
+                print "Entity tag mismatch.\n";
+                return;
+            }
+        }
+        $del = @unlink($p.$fn);
+        if(!$del) {
+            header('HTTP/1.1 500 Internal Server Error');
+            header('Content-Type: text/plain');
+            print "Could not delete.\n";
+        }
+        header('HTTP/1.1 200 OK');
+        header('Content-Type: text/plain');
+        print "Deleted.\n";
+    } else {
+        header('HTTP/1.1 404 Not Found');
+        header('Content-Type: text/plain');
+        print "File not found.\n";
+    }
+}
+
 ob_start();
+// globals.
 $method = strtolower(CLI ? isset($argv[2]) ? $argv[2] : 'get' : $_SERVER['REQUEST_METHOD']);
 $uri = CLI ? isset($argv[1]) ? $argv[1] : condition_uri('/') : condition_uri($_SERVER['REQUEST_URI']);
+$input_stream = CLI ? 'php://stdin' : 'php://input';
 $hdrs = array();
 
 if(!valid_http_method($method)) {
